960e1f7332c62fa7364d27a50d1fd85f256ad35d
Aero <strixzz39@gmail.com>
2025-12-11 05:34:10 +0000

    Initial Commit

---
+++ XString/XStringApp.swift
@@ -0,0 +1,17 @@
+//
+//  XStringApp.swift
+//  XString
+//
+//  Created by AeroStar on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct XStringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
